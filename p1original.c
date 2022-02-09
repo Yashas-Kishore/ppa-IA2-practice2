108dadb26950ec62ee9cf268635b94eb61c48a62
Yashas Kishore <95675075+Yashas-Kishore@users.noreply.github.com>
2022-02-09 06:39:26 +0000

    2 r done

--- p1original.c
+++ p1original.c
@@ -1,2 +1,30 @@
+#include<stdio.h>
 
+void input(float *base, float *height)
+{
+  printf("enter the base and the height respectively: \n");
+  scanf("%f%f",base,height);
+
+}
+
+
+void find_area(float base , float height, float *area)
+{
+  float a;
+  a=(base*height);
+  *area=a/2;
+}
+void output(float base, float height, float area)
+{
+  printf("the area of triangle with the given base %f and height %f is %f",base,height,area);
+
+}
+
+int main()
+{
+  float b,h,area;
+  input(&b,&h);
+  find_area(b,h,&area);
+  output(b,h,area);
+  return 0;
 }
---
+++ p2original.c
@@ -0,0 +1,70 @@
+#include<stdio.h>
+
+int input_side()
+{
+  int a;
+  printf("enter the side of the triangle : \n");
+  scanf("%d",&a);
+  return a;
+}
+int check_scalene(int a, int b, int c)
+{
+  int tri;
+  if (a==b && b==c)
+  {
+  tri=0;
+  }
+  else if(a!=b && a==c) 
+  {
+    tri=1;
+  }
+  else if(a==b && a!=c)
+  {
+    tri=2;
+  }
+  else if(b==c && a!=b)
+  {
+    tri=3;
+  }
+  else 
+  {
+    tri=5;
+  }
+  return tri;
+}
+void output(int a, int b, int c, int tri)
+{
+  
+  if(tri==0)
+  {
+  printf(" is equilateral ");
+  }
+  if(tri==1)
+  {
+  printf(" is isosceles ");
+  }
+  if(tri==2)
+  {
+  printf(" is isosceles ");
+  }
+  if(tri==3)
+  {
+  printf(" is isosceles ");
+  }
+  else if(tri==5)
+  {
+    printf("is scalene");
+  }
+}
+
+int main()
+{
+  int m,n,o,y;
+  m=input_side();
+  n=input_side();
+  o=input_side();
+  y=check_scalene(m,n,o);
+  output(m,n,o,y);
+  return 0;
+
+}
